daff6d69289b1aa9051e7b10cda9463a768a4897
Ricardo Oneda <20815222+roneda@users.noreply.github.com>
2023-08-08 15:09:16 +0000

    Criação do devcontainer e arquivos de componentes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component that should produce HTML
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Show it on the page (DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
